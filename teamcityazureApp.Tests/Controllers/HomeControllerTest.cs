5b2e9e3a35b74164ab79e9fed476bf2d3b0d69b0
teamcityazure <teamcityazure@gmail.com>
2017-05-15 00:54:14 +0300

    test1

--- teamcityazureApp.Tests/Controllers/HomeControllerTest.cs
+++ teamcityazureApp.Tests/Controllers/HomeControllerTest.cs
@@ -48,7 +48,7 @@ namespace teamcityazureApp.Tests.Controllers
             ViewResult result = controller.Contact() as ViewResult;
 
             // Assert
-            Assert.IsNotNull(result);
+            Assert.IsNotNull(null);
         }
     }
 }
